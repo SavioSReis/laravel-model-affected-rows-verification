047141012c0510eed73254ec8865fa4ebd7a5b1c
SavioAmin <87980098+SavioSReis@users.noreply.github.com>
2024-11-08 10:34:39 -0300

    Remove unused import for RowCountFailVerificationException in Model.php

--- src/Model.php
+++ src/Model.php
@@ -4,7 +4,6 @@ namespace MobileStock\Laravel\Model\AffectedRowsVerification;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
-use MobileStock\Laravel\Model\AffectedRowsVerification\Exception\RowCountFailVerificationException;
 
 /**
  * @method static static create(array $attributes = [])
